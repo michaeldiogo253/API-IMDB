b11d48011f93c453e2ce250ec6f78286534dc96f
Michael Diogo Fagundes <michael@petize.com.br>
2022-11-20 10:27:18 -0300

    initial commit

---
+++ src/test/java/br/com/impdapi/ImpbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.impdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
