4a466eba5773ec57bc02e274db8b22d955138e6b
Michael Diogo Fagundes <michael@petize.com.br>
2022-11-20 10:33:39 -0300

    refactor! adicao de path variable em end point

--- src/main/java/br/com/impdapi/controller/ListarTop250FilmesImdbController.java
+++ src/main/java/br/com/impdapi/controller/ListarTop250FilmesImdbController.java
@@ -3,10 +3,9 @@ package br.com.impdapi.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.URISyntaxException;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,14 +13,12 @@ public class ListarTop250FilmesImdbController {
 
 	private final RestTemplate restTemplate;
 
-	@GetMapping("/listar-top-250-filmes")
-	public ResponseEntity<String> listarTop250filmes() throws URISyntaxException {
+	@GetMapping("/listar-top-250-filmes/{apiKey}")
+	public ResponseEntity<String> listarTop250filmes(@PathVariable String apiKey) {
 
-		String  url ="https://imdb-api.com/en/API/Top250Movies/k_gbfa09ou";
+		String  url ="https://imdb-api.com/en/API/Top250Movies/"+ apiKey;
 
-		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-		return response;
+		return restTemplate.getForEntity(url, String.class);
 	}
 
 }
